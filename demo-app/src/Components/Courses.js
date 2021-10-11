462d3e3508b3dd6798ce8b3d8e308e9b02c308bd
PVArias <paulavictoriaarias@mail.com.ar>
2021-10-11 17:46:24 -0300

    segundo commmit cursos

--- demo-app/src/Components/Courses.js
+++ demo-app/src/Components/Courses.js
@@ -7,24 +7,21 @@ const Courses = () => {
     return(
         <div>
             <div className="main-div">
-                <h1 className="custom-title"> Crea tu propia Manga</h1>
-                <p className="description">Cursos que te ayudarán a crear tu propia Manga</p>
+                <h1 className="custom-title"> Convertite en Mangaka!</h1>
+                <p className="description">Aprende desde cero a crear tus propios personajes y tus propias historias con estilo manga.</p>
             </div>
             {subjets.map((subjet) => {
                 return (
                     <div className="contents">
-                        <h2>{subjet}</h2>
-                        <div className="row-picture-desc">
-                            <img src="aprender-programacion-web.jpg" className="picture"/>
-                            <p className="text1">
-                                En este curso se veran diversos contenidos vinculados con la programación web, como por
-                                ejemplo
-                                JavaScript, CSS, React, Django y algunos otros conceptos avanzados.
-                            </p>
-                        </div>
-                        <h3 className="price">
-                            Precio: $1000
-                        </h3>
+                            <h2 className="title">{subjet}</h2>
+                            <div className="row-picture-desc">
+                                <p className="text1">
+                                    Curso para principiantes, donde aprenderás las técnicas del dibujo  japonés para la creación de tu propia manga.
+                                </p>
+                            </div>
+                            <div className ="button">
+                                <button type="button" className="btn btn-outline-danger">+Info</button>
+                            </div>
                     </div>
                 )})}
         </div>
