84666d83de6fe92cadaba112ebfb30db6e985567
MaritaMarita <mjcardinale@mail.austral.edu.ar>
2021-11-13 11:59:05 -0300

    views, urls, drf_yasg, swagger

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,9 +2,11 @@ from django.urls import path, include
 from rest_framework import routers
 
 from api.views import CourseViewSet
+from api.views import GalleryViewSet
 
 router = routers.DefaultRouter()
 router.register(r'courses', CourseViewSet)
+router.register(r'gallery', GalleryViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
