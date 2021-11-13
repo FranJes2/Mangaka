fb7bfe37168f45e3fb029554b553d7840b4f491a
MaritaMarita <mjcardinale@mail.austral.edu.ar>
2021-11-12 21:26:27 -0300

    gitignore y urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,3 +1,11 @@
-urlpatterns = [
+from django.urls import path, include
+from rest_framework import routers
 
+from api.views import CourseViewSet
+
+router = routers.DefaultRouter()
+router.register(r'courses', CourseViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
 ]
