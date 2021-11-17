5a0d1b8258c9ee8bf3550d41b1c9f699ed10d22a
MaritaMarita <mjcardinale@mail.austral.edu.ar>
2021-11-17 12:09:40 -0300

    cambios del lunes y martes

--- demo-app/src/Components/Presentación/Courses.js
+++ demo-app/src/Components/Presentación/Courses.js
@@ -1,48 +1,76 @@
 import './courses.css';
 import {useEffect, useLayoutEffect, useState} from "react";
 import {Link} from "react-router-dom";
-import * as url from "url";
-import {httpGet} from "../../utils/httpFunction";
+import {httpGet, httpPost} from "../../utils/httpFunction";
 
 
 const Courses = () => {
 
-    const subjects = ['Curso Principiantes', 'Curso Intermedio', 'Curso Avanzado']
+    const [courses, setCourses] = useState([])
 
-    const [courses, setCourses ] = useState( initialState: [])
+    const [name, setName] = useState([])
+    const [description, setDescription] = useState([])
+    const [price, setPrice] = useState([])
 
     const fetchCourses = () => {
         httpGet('api/courses/')
-            .then((data :void) = setCourses(data))
+            .then((res) => setCourses(res.data))
     }
+
+    const createCourse = () => {
+        httpPost('api/courses/', {name: name, description: description, price: price})
+            .then(fetchCourses)
+    }
+
     useEffect(fetchCourses, [])
 
 
-
-    return(
+    return (
         <div className="Sekai">
             <div className="main-div">
                 <h1 className="custom-title"> Convertite en Mangaka!</h1>
-                <p className="description">Aprende desde cero a crear tus propios personajes y tus propias historias con estilo manga.</p>
+                <p className="description">Aprende desde cero a crear tus propios personajes y tus propias historias con
+                    estilo manga.</p>
             </div>
-            {subjects.map((subject) => {
-                return (
-                    <div className="contents">
-                            <h2 className="title">{subject}</h2>
-                            <div className="row-picture-desc">
-                                <p className="text1">
-                                    Curso para principiantes, donde aprenderás las técnicas del dibujo  japonés para la creación de tu propia manga.
-                                </p>
-                            </div>
-                            <div className ="button">
-                                <Link to={`/Courses/detail/${subject}`}><button type="button" className="btn btn-outline-danger">+Info</button></Link>
-                            </div>
-                    </div>
-                )})}
-        </div>
-    )
-}
 
+            <div className="main-div">
+                <form onSubmit={createCourse}>
+                    <fieldset>
+                        <legend>Crear un nuevo curso</legend>
+                        <div className="mb-3">
+                            <label htmlFor="disabledTextInput" className="form-label">Name</label>
+                            <input type="text" id="disabledTextInput" className="form-control" value={name}
+                                   onChange={(e) => setName(e.target.value)}/>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="disabledTextInput" className="form-label">Description</label>
+                            <input type="text" id="disabledTextInput" className="form-control" value={description}
+                                   onChange={(e) => setDescription(e.target.value)}
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="disabledTextInput" className="form-label">Price</label>
+                            <input type="text" id="disabledTextInput" className="form-control" value={price}
+                                   onChange={(e) => setPrice(e.target.value)}
+                            />
+                        </div>
+                        <button type="submit" className="btn btn-primary">CREAR CURSO</button>
+                    </fieldset>
+                </form>
+            </div>
+
+            <div className="all-cards">
+                {
+                    finalSubjects
+                        .map((mapSubject) => {
+                            return (
+                                <SubjectCard subject={mapSubject}/>
+                            )
+                        })
+                }
+            </div>
+        </div>)
+}
 
 
 export default Courses
--- demo-app/src/utils/httpFunction.js
+++ demo-app/src/utils/httpFunction.js
@@ -1,13 +1,12 @@
-import axios from "axios";
-import url from "url";
-
 const axios = require('axios');
 
 const baseUrl = 'http://127.0.0.1:8000/'
 
 export const httpGet = async (endpoint) => {
-    axios.get( baseUrl + endpoint)
-      .then((res) => {
-        return res.data
-    })
-}
+   return axios.get( baseUrl + endpoint)
+    }
+
+export const httpPost = async (endpoint, data) => {
+    return axios.post( baseUrl + endpoint, data)
+    }
+
