fef2a57a7d0898683c1f5bf7495e9eb641c6081e
MaritaMarita <mjcardinale@mail.austral.edu.ar>
2021-11-23 15:34:49 -0300

    corsheaders

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cors_headers'
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg',
