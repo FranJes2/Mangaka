604f05fdd117aeb857439ae694d503ada6b558d1
MaritaMarita <mjcardinale@mail.austral.edu.ar>
2021-12-16 19:47:02 -0300

    saqué $

--- backend/api/views.py
+++ backend/api/views.py
@@ -13,12 +13,15 @@ class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
 
+
     def get_queryset(self):
         queryset = self.queryset
-        course_name = self.request.query_params.get('name')
-        if course_name is not None:
-            queryset = queryset.filter(name=course_name)
+        course_topprice = self.request.query_params.get('topprice')
+        if course_topprice is not None:
+            queryset = queryset.filter(price__lte=course_topprice)
         return queryset
+
+
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
